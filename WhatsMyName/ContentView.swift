a4c124851474d13a84d1cc2025d8162fa8ed8708
Bryan <Bryan>
2020-08-26 08:44:37 -0400

    Initial Commit

---
+++ WhatsMyName/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsMyName
+//
+//  Created by User23198271 on 8/26/20.
+//  Copyright © 2020 Bryan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
